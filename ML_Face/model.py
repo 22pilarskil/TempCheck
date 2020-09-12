af375e285558175b0ed493b8a46b20273b96f67b
Liam <22pilarskil@millburn.org>
2020-09-12 13:49:33 -0400

    Combine thermal and .py and model.py

--- ML_Face/model.py
+++ ML_Face/model.py
@@ -3,6 +3,20 @@ import numpy as np
 import cv2
 import os
 
+import sys
+from pylepton.Lepton3 import Lepton3
+
+#mtcnn = MTCNN()
+
+def capture(flip_v = False, device = "/dev/spidev0.1"):
+  with Lepton3(device) as l:
+    a,_ = l.capture()
+  if flip_v:
+    cv2.flip(a,0,a)
+  cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
+  np.right_shift(a, 8, a)
+  return np.uint8(a)
+
 os.system("sh model_download.sh")
 
 def build_model(model_path, output_tensor_names, input_tensor_name, session="tf", placeholder=None):
@@ -34,25 +48,24 @@ model = build_model(
         placeholder=(1, None, None, 3)
         )
 
-img = cv2.imread("image.jpg")
+while(True):
+    image = capture(flip_v = False, device = "/dev/spidev0.1")
+    expanded = np.expand_dims(img, axis=0)
 
-expanded = np.expand_dims(img, axis=0)
+    boxes, scores, classes = list(model["sess"].run(model["output_tensors"], feed_dict={model["input_tensor"]: expanded}))
+    combined = zip(np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes))
+    bounds = []
+    boxes, scores, classes = zip(*(list(filter(lambda x: sum(x[0]) > 0 and x[2] == 1 and x[1] > .5, combined)))) 
+    for box in boxes:
+        bounds.append([int(box[i] * img.shape[i%2]) for i in range(len(box))])
 
-boxes, scores, classes = list(model["sess"].run(model["output_tensors"], feed_dict={model["input_tensor"]: expanded}))
-
-combined = zip(np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes))
-bounds = []
-boxes, scores, classes = zip(*(list(filter(lambda x: sum(x[0]) > 0 and x[2] == 1 and x[1] > .5, combined)))) 
-for box in boxes:
-    bounds.append([int(box[i] * img.shape[i%2]) for i in range(len(box))])
-
-for bound in bounds:
-    color=(0,255,0) 
-    cv2.rectangle(img, (bound[1],bound[0]), (bound[3],bound[2]), color=color, thickness=4)
-
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-
-print(boxes)
-print(scores)
-print(classes)
+    for bound in bounds:
+        color=(0,255,0) 
+        cv2.rectangle(img, (bound[1],bound[0]), (bound[3],bound[2]), color=color, thickness=4)
+        #results = mtcnn.detect_faces(image)
+        #for result in results:
+      #box = result['box']
+      #cv2.rectangle(image, (box[0], box[1]),(box[0]+box[2], box[1]+box[3]), color=(0, 255, 0), thickness=4)
+    cv2.imshow("Image", image)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+      break
