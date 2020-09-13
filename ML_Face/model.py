336d9b1d216f8b04822b93c19a03036fab52d393
Liam Pilarski <22pilarskil@millburn.org>
2020-09-12 21:56:42 -0400

    Local changes

--- ML_Face/model.py
+++ ML_Face/model.py
@@ -2,20 +2,35 @@ import tensorflow as tf
 import numpy as np 
 import cv2
 import os
-
+import time
+from sklearn.neighbors import NearestNeighbors
 import sys
 from pylepton.Lepton3 import Lepton3
-
+import pymongo
+import datetime as dt
+#from pylepton import Lepton
 #mtcnn = MTCNN()
+
+CameraID = "824209"
+
+pw="hack"
+user="Liam-Pilarski"
+
+connectionURL = f"mongodb+srv://{user}:{pw}@tempcheck.cfwko.mongodb.net/<dbname>?retryWrites=true&w=majority"
+client = pymongo.MongoClient(connectionURL)
+mydb=client["LeptonData"]
+mainCollection=mydb["DATA"]
 
 def capture(flip_v = False, device = "/dev/spidev0.1"):
   with Lepton3(device) as l:
     a,_ = l.capture()
+    b,_ = l.capture()
   if flip_v:
     cv2.flip(a,0,a)
   cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
   np.right_shift(a, 8, a)
-  return np.uint8(a)
+  print(b)
+  return np.uint8(a), b
 
 os.system("sh model_download.sh")
 
@@ -37,7 +52,7 @@ def calc_overlap(bounds):
         (max(rect1[2], rect2[2]) - max(rect1[0], rect2[0])))
     overlap_percentage = overlap_area / min(rect1_area, rect2_area)
 
-    if not len(bounds) == 1 and (overlap_percentage > .5 or max(rect1_area, rect2_area) / min(rect1_area, rect2_area) > 1.3):
+    if len(bounds) > 2 and (overlap_percentage > .5 or max(rect1_area, rect2_area) / min(rect1_area, rect2_area) > 1.3):
         del(bounds[0 if rect1_area < rect2_area else 1])
         calc_overlap(bounds)
     else: 
@@ -73,26 +88,57 @@ model = build_model(
         )
 
 while(True):
-    image = capture(flip_v = False, device = "/dev/spidev0.1")
+    img, temp = capture(flip_v = False, device = "/dev/spidev0.1")
+    print(img.shape)
+    print(temp.shape)
+    print('next')
     expanded = np.expand_dims(img, axis=0)
-    stretched = np.repeat(expanded, 3, axis=4)
+    stretched = np.repeat(expanded, 3, axis=3)
+    temperatures = []
+    peopleData = []
 
     boxes, scores, classes = list(model["sess"].run(model["output_tensors"], feed_dict={model["input_tensor"]: stretched}))
     combined = zip(np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes))
     bounds = []
-    boxes, scores, classes = zip(*(list(filter(lambda x: sum(x[0]) > 0 and x[2] == 1 and x[1] > .5, combined)))) 
-    for box in boxes:
-        bounds.append([int(box[i] * img.shape[i%2]) for i in range(len(box))])
+    try:
+        boxes, scores, classes = zip(*(list(filter(lambda x: sum(x[0]) > 0 and x[2] == 1 and x[1] > .5, combined)))) 
+        for box in boxes:
+            bounds.append([int(box[i] * img.shape[i%2]) for i in range(len(box))])
+        bounds = calc_overlap(bounds) if len(bounds) > 1 else bounds
+        if bounds:
+            for bound in bounds:
+                color=(0,255,0) 
+                cv2.rectangle(img, (bound[1],bound[0]), (bound[3],bound[2]), color=color, thickness=4)
+        for bound in bounds:
+            temperatures.append(temp.flatten()[0])
+            temp_ = temp[bound[0]:bound[2], bound[1]:bound[3]].flatten()
+            toptenpercent = np.mean(temp_[np.argsort(temp_)[-int(len(temp.flatten())*.1):]])
+            calculation = (toptenpercent*0.01)-273.15
+            print(calculation)
+            # calculation = sum(toptenpercent)/len(toptenpercent)
+            # faceAverage = sum(temp_)/len(temp_)
+            # print(calculation)
+            # print(faceAverage)
+            time.sleep(4)
+            isSick = False
+            if calculation >= 38:
+                isSick = True
+            peopleData.append([calculation, isSick])
+        
+        final = {
+            "CameraID":CameraID,
+            "PeopleData":peopleData,
+            "NumbOfPeople":len(peopleData),
+        }
+        if mainCollection.find_one({"CameraID":CameraID}):
+            mainCollection.replace({"CameraID":CameraID}, final)
+        else:
+            mainCollection.insert_one(final)
 
-    bounds = calc_overlap(bounds) if len(bounds) > 1 else bounds
-
-    for bound in bounds:
-        color=(0,255,0) 
-        cv2.rectangle(img, (bound[1],bound[0]), (bound[3],bound[2]), color=color, thickness=4)
-        #results = mtcnn.detect_faces(image)
-        #for result in results:
-      #box = result['box']
-      #cv2.rectangle(image, (box[0], box[1]),(box[0]+box[2], box[1]+box[3]), color=(0, 255, 0), thickness=4)
-    cv2.imshow("Image", image)
+        
+    except (ValueError, TypeError) as e:
+        print(e)
+    
+    cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
-      break
+        break
